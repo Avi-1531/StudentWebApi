d11de54af48aaa56d03205a18321b7d4ab462c92
Avi Chauhan <117462878+Avi-1531@users.noreply.github.com>
2025-08-12 04:43:22 +0000

    Update Program.cs

--- StudentWebApi/Program.cs
+++ StudentWebApi/Program.cs
@@ -43,11 +43,15 @@ builder.Services.AddDbContextPool<StudentDB>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
